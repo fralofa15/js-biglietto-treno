6795b7c14883c75eb13715f2fc84761b94a245cd
Lorenzo Favale <lorenzofavale150420@gmail.com>
2026-03-24 16:12:57 +0100

    aggiunta del controllo di età per l'applicazione degli sconti e la stampa del prezzo finale del biglietto

--- script.js
+++ script.js
@@ -11,3 +11,19 @@ const prezzoAlKm = 0.21;
 
 // Calcolo del prezzo iniziale (senza sconti)
 let prezzoBiglietto = km * prezzoAlKm;
+
+// applico uno sconto del 20%
+if (eta < 18) {
+    prezzoBiglietto = prezzoBiglietto * 0.8;
+} 
+// applico uno sconto del 40%
+else if (eta > 65) {
+    prezzoBiglietto = prezzoBiglietto * 0.6;
+}
+// Se non rientra in nessuna categoria di sconto
+else {
+    console.log("Nessuno sconto")
+}
+
+// Stampo il prezzo finale
+console.log(`Il prezzo finale del biglietto è €${prezzoBiglietto.toFixed(2)}`);
